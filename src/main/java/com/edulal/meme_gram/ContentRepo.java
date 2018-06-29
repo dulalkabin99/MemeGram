6ba7d1702d6d9b880a75cfaed2f7d552c73e4736
Kabin Dulal <dulalkabin99@gmail.com>
2018-06-29 15:54:47 -0400

    first commit

---
+++ src/main/java/com/edulal/meme_gram/ContentRepo.java
@@ -0,0 +1,6 @@
+package com.edulal.meme_gram;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ContentRepo extends CrudRepository<MemeGram, Long> {
+}
---
+++ src/test/java/com/edulal/meme_gram/MemeGramApplicationTests.java
@@ -0,0 +1,16 @@
+package com.edulal.meme_gram;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemeGramApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
